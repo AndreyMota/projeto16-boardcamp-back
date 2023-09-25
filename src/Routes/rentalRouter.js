1f22c07a952c7dea3d07085517dfb97bd03f6924
AndreyMota <andreymotam1@gmail.com>
2023-09-25 16:09:51 -0300

    end retal post test 2

--- src/Routes/rentalRouter.js
+++ src/Routes/rentalRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import validateSchema from "../Middlewares/validateSchema.js";
 import { RentalSchema } from "../Schemas/rentalSchema.js";
-import { postRental, getRentals } from "../Controllers/rentalController.js";
+import { postRental, getRentals, endRental } from "../Controllers/rentalController.js";
 
 const rentalRouter = Router();
 
 rentalRouter.post('/rentals', validateSchema(RentalSchema), postRental);
+rentalRouter.post('/rental/:id/return', endRental);
 rentalRouter.get('/rentals', getRentals);
 
 export default rentalRouter;
